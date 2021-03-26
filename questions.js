fac2d9e2781b183fa4bd83f2c12deb9ba6d89eb1
sana chowdhury:git config --global user.name sana <sana.chowdhury86@gmail.com>
2021-03-25 21:02:12 -0400

    HTML for second page and javascript for questions component

--- questions.js
+++ questions.js
@@ -5,7 +5,7 @@ const choiceElem = document.getElementById('multichoices');
 const option1 = document.querySelector('#option1')
 const option2 = document.querySelector('#option2')
 const option3 = document.querySelector('#option3')
-
+const showResultElem = document.querySelector('#result')
 
 
 //questions
