5a48cbb0f5470f1aad9a1b2fe8893b0327645474
Fasica Robi <fasicasr@gmail.com>
2021-04-02 00:48:09 -0400

    edited questions

--- questions.js
+++ questions.js
@@ -1,9 +1,9 @@
 // DOM SELECTORS
 const assessForm = document.getElementById("assessmentForm")
 const question1 = document.getElementById('question1')
-const question2 = Array.from(document.querySelectorAll('input[name = "experience"]'))
-const question3 = Array.from(document.querySelectorAll('input[name = "term"]'))
-const question4 = Array.from(document.querySelectorAll('input[name = "invest"]'))
+//const question2 = Array.from(document.querySelectorAll('input[name = "experience"]'))
+const question2 = Array.from(document.querySelectorAll('input[name = "term"]'))
+const question3 = Array.from(document.querySelectorAll('input[name = "invest"]'))
 const submitBtn = document.getElementById('submitComplete')
 console.log(question1.value)
 // FUNCTIONS
@@ -14,9 +14,9 @@ const submitForm = e => {
   let answer1
   let answer2
   let answer3
-  let answer4
   
   answer1 = question1.value
+  console.log(question1.value)
   question2.forEach(option => {
     if(option.checked) {
       answer2 = option.id
@@ -27,31 +27,26 @@ const submitForm = e => {
       answer3 = option.id
     }
   })
-  question4.forEach(option => {
-    if(option.checked) {
-      answer4 = option.id
-     
-    }
-  })
 
+  //
  
   //let coinName; <--- coinName needs a value after the assessment is done
-  let coinName1 = 'bitcoin';
+  let coinName1 = question1.value
   let coinName2 = 'dogecoin';
   localStorage.setItem('coinName1', coinName1)
   localStorage.setItem('coinName2', coinName2)
   window.location.assign("./cryptocur.html")
 
 
-  // MAKE A FETCH FROM API
-  const url = `https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=${answer1}`
-  fetch(url)
-    .then(res => res.json())
-    .then(data => {
-    console.log("data", data)
-    localStorage.setItem ('answer1', 'answer2', 'answer3', data);
-    window.location.replace("./cryptocur.html")
-    })
+  // // MAKE A FETCH FROM API
+  // // const url = `https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=${answer1}`
+  // // fetch(url)
+  // //   .then(res => res.json())
+  // //   .then(data => {
+  // //   console.log("data", data)
+  // //   localStorage.setItem ('answer1', 'answer2', 'answer3', data);
+  //   window.location.replace("./cryptocur.html")
+  //   })
 
 }
 
@@ -61,27 +56,31 @@ const submitForm = e => {
 submitBtn.addEventListener('click', function(e){
   // Run validation
   e.preventDefault()
-  let formIsValid = false
-  
+
+  let formIsValid = 0; 
+  //If question 1 value is not empty then user has selected a coin
+  if (question1.value !== "") {
+      formIsValid++ 
+  }
+  //console.log("checking q2")
+  for (let i = 0; i < question2.length; i++) {
+    if (question2[i].checked == true) {
+      formIsValid++
+    }
+    
+  } 
+  //console.log("checking q3")
+  //console.log("the form is valid = "+formIsValid)
   for (let i = 0; i < question3.length; i++) {
     if (question3[i].checked == true) {
-      formIsValid = true
-    
+      formIsValid++ 
     }
     
-  } console.log("checking q3")
+  }
+
   console.log("the form is valid = "+formIsValid)
-  for (let i = 0; i < question4.length; i++) {
-    if (question4[i].checked == true) {
-      formIsValid = true 
-    
-    }
-    
-  }console.log("checking q4")
-  console.log("the form is valid = "+formIsValid)
-    if (formIsValid === true) {
+    if (formIsValid === 3) {
      submitForm()
-      
     }
 
 }) 
